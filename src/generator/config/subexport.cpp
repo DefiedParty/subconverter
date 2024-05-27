273c542f3519bf9685d0d5714125cf284a001ae3
DefiedParty <44311872+DefiedParty@users.noreply.github.com>
2024-05-27 19:10:32 +0000

    Add remote-dns-resolve field for wireguard.

--- src/generator/config/subexport.cpp
+++ src/generator/config/subexport.cpp
@@ -477,8 +477,8 @@ void proxyToClash(std::vector<Proxy> &nodes, YAML::Node &yamlnode, const ProxyGr
                 singleproxy["dns"] = x.DnsServers;
             if(x.Mtu > 0)
                 singleproxy["mtu"] = x.Mtu;
-            if(!x.RemoteDnsResolve.indeterminate())
-                singleproxy["remote-dns-resolve"] = x.RemoteDnsResolve;
+            if(!x.RemoteDnsResolve.is_undef())
+                singleproxy["remote-dns-resolve"] = x.RemoteDnsResolve.get();
             break;
         default:
             continue;
